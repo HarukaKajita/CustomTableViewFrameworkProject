45c77051df98d44a458610c4be6138a92be46834
梶田悠 <kajitaj63b3@gmail.com>
2018-05-06 06:34:58 +0900

    framework書き出しで出力されたフォルダをframeworkフォルダに入れました。

---
+++ framework/Debug-iphoneos/CustomTableViewFramework.framework/Headers/CustomTableViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewFramework.h
+//  CustomTableViewFramework
+//
+//  Created by 梶田悠 on 2018/05/06.
+//  Copyright © 2018年 kajita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomTableViewFramework.
+FOUNDATION_EXPORT double CustomTableViewFrameworkVersionNumber;
+
+//! Project version string for CustomTableViewFramework.
+FOUNDATION_EXPORT const unsigned char CustomTableViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomTableViewFramework/PublicHeader.h>
+
+
